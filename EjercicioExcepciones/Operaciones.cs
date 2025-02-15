d5ef360353e8ce088292220a1a61e32662415d9e
Allitan <allanmendoza036@gmail.com>
2025-02-14 21:54:57 -0600

    Create Operaciones.cs
    Creando la clase Operaciones

---
+++ EjercicioExcepciones/Operaciones.cs
@@ -0,0 +1,29 @@
+﻿
+namespace EjercicioExcepciones
+{
+    public class Operaciones
+    {
+        public double Suma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public double Resta(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiplicacion(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Dividir(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+                throw new DivideByZeroException("No se divide entre 0");
+            }
+            return n1 / n2;
+        }
+    }
+}
